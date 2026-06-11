2c36a20dee58afc830b6c3f5b4e2f702f38fa2a1
SATABDIDEHURY <satabdi772000@gmail.com>
2026-06-11 09:27:30 +0530

    subscription service devloping

--- subscription-service/src/main/java/com/myProject/subscription_service/entity/Subscription.java
+++ subscription-service/src/main/java/com/myProject/subscription_service/entity/Subscription.java
@@ -1,7 +1,20 @@
 package com.myProject.subscription_service.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
 
 @Entity
 public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long sub_Id;
+    @JoinColumn(nullable = false)
+    private String userId;
+    @ManyToOne
+    @JoinColumn(name = "plan_id", nullable = false)
+    private SubscriptionPlan plan;
+    private String status;
+    private LocalDate startDate;
+    private LocalDate endDate;
 }
